f3e9f2a68358e376e7d663e96d537dd36e189b4c
Chinnawat K <chinnawatsut@gmail.com>
2017-10-20 17:21:05 +0700

    tuple

---
+++ 03_basic_tuple/main.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+pi_tuple = (3,1,4,5,6)
+print(pi_tuple)
+new_tuple = list(pi_tuple)
+print(new_tuple)
+new_list = tuple(pi_tuple)
+print(new_list)
+
+
+print(len(pi_tuple))
+print(min(pi_tuple))
+print(max(pi_tuple))
