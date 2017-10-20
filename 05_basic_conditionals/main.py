1d16dfb1a2e17f2962fd5bdd2ad0bed8f4af37f5
Chinnawat K <chinnawatsut@gmail.com>
2017-10-20 17:37:41 +0700

    conditionals

---
+++ 05_basic_conditionals/main.py
@@ -0,0 +1,25 @@
+import sys
+import os
+
+age = 31
+
+if age > 16 :
+    print("You\'re old enough to drive")
+else :
+    print("You\'re not old enough to drive")
+
+if age >= 21:
+    print('You\'re old enough to drive a tractor trailer')
+elif age >= 16:
+    print('You\'re old enough to drive a car')
+else :
+    print('You\'re not old enough to drive ')
+
+if ((age >= 1) and (age <= 18)):
+    print("You get a birthday")
+elif (age == 21) or (age >= 65):
+    print("You get a birthday")
+elif  not (age == 30):
+    print("You don't get a birthday")
+else:
+    print("You get a birthday party yeah")
